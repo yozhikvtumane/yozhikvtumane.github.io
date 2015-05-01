1534fc99e06a7c9ea29baa871aa34b0a3c8de169
yozhikvtumane <toshamatveeev@gmail.com>
2015-05-01 16:33:07 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$(document).on('click' , function(menuSwitch){
+		menuSwitch.stopPropagation();
+		if (menuSwitch.target.id == 'menuSwitch') {
+			$(".header__menu").toggleClass("header__menu_visible");
+			$(".header__photo").toggleClass("header__photo_invisible");
+		} else {
+			$(".header__photo").removeClass("header__photo_invisible");
+			$(".header__menu").removeClass("header__menu_visible");
+		}
+	});
+});

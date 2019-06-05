67bf28fa21bb38f0ddf12bc6107b55bde0e52418
yozhikvtumane <yozhikvtumane@users.noreply.github.com>
2019-06-05 17:09:06 +0100

    upd

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
-const navBar = document.getElementById('navbar')
+const header = document.querySelector('header')
 const copy = document.querySelector('.copy')
+
 window.addEventListener('scroll', ()=>{
-    if (window.scrollY > 180) {
-        navBar.style.padding = '0.7em 5em'
+    if (window.scrollY > 60) {
+        header.style.paddingTop = '0.5em'
+        header.style.paddingBottom = '0.5em'
     } else {
-        navBar.style.padding = '2em 5em'
+        header.style.paddingTop = '1.5em'
+        header.style.paddingBottom = '1.5em'
     }
 })
 
 let y = new Date().getFullYear()
-copy.textContent = 'My personal portfolio © ' + y
+copy.textContent = 'My personal portfolio | ' + y
 

38439d07b2fe3551f3bfca079dcae07c008b22f1
yozhikvtumane <yozhikvtumane@users.noreply.github.com>
2019-06-08 13:06:21 +0100

    upd

--- js/index.js
+++ js/index.js
@@ -35,8 +35,7 @@ window.addEventListener('scroll', ()=>{
 
 Array.from(moreButtons).map(button => button.addEventListener('click' , showMoreText))
 phone.addEventListener('click', showPhoneNumber)
-// phone.addEventListener('click', showPhoneNumber)
-// window.
+
 
 let y = new Date().getFullYear()
 copy.textContent = ` |${y}`
